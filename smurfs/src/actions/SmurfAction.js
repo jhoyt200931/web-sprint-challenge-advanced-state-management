a413ba2aab47d77b4b4182415e9c89bec99b09f7
Johnathan Hoyt <johnathanhoyt@Gabriellas-MacBook-Pro.local>
2020-09-26 14:41:21 -0400

    rendered data to page, added form to add smurf, working on add smurf functionality

--- smurfs/src/actions/SmurfAction.js
+++ smurfs/src/actions/SmurfAction.js
@@ -4,6 +4,8 @@ import axios from 'axios';
 export const ADD_SMURF = 'ADD_SMURF';
 export const GET_SMURFS_START = 'GET_SMURFS_START';
 export const GET_SMURFS_SUCCESS = 'GET_SMURFS_SUCCESS';
+export const GET_SMURFS_FAIL = 'GET_SMURFS_FAIL';
+export const UPDATE_SMURF = 'UPDATE_SMURF';
 
 
 
@@ -12,12 +14,23 @@ export const getSmurfs = () => (dispatch) => {
     axios.get('http://localhost:3333/smurfs')
         .then(res => {
             console.log(res);
-            // dispatch({type: GET_SMURFS_SUCCESS, payload: })
+
+            dispatch({type: GET_SMURFS_SUCCESS, payload: res.data.map(item => {
+                return item;
+            }) })
+        })
+        .catch(err => {
+            dispatch({type: GET_SMURFS_FAIL, payload: err.message})
         })
 }
 
 
-
+export const updateSmurf = (e) => (dispatch) => {
+    dispatch({type: UPDATE_SMURF, payload: {
+        targetName: e.target.name,
+        targetValue: e.target.value
+    }})
+}
 
 // export const addSmurf = () => {
 //     return
--- smurfs/src/reducers/SmurfReducer.js
+++ smurfs/src/reducers/SmurfReducer.js
@@ -1,15 +1,50 @@
-
+import { GET_SMURFS_START, GET_SMURFS_SUCCESS, GET_SMURFS_FAIL, UPDATE_SMURF } from '../actions/SmurfAction';
 
 const initialState = {
-    name: '',
-    age: 1,
-    height: '',
-    id: 1
+    smurfs: [],
+    isGettingSmurfs: false,
+    error: '',
+    newSmurf: {
+        name: '',
+        age: 1,
+        height: '',
+        id: 1
+    }
 }
 
 
 export const SmurfReducer = (state = initialState, action) => {
     switch(action.type) {
+        case GET_SMURFS_START:
+            return {
+                ...state,
+                isGettingSmurfs: true
+            }
+        case GET_SMURFS_SUCCESS:
+            return {
+                ...state,
+                smurfs: [
+                    ...state.smurfs,
+                    action.payload
+                ],
+                isGettingSmurfs: false
+            }    
+        case GET_SMURFS_FAIL:
+            return {
+                ...state,
+                error: action.payload,
+                isGettingSmurfs: false
+            }
+        case UPDATE_SMURF: 
+            return {
+                ...state,
+                
+
+                newSmurf: {
+                    ...state.newSmurf,
+                    [action.payload.targetName]: action.payload.targetValue
+                }
+            }
 
         default: 
         return state;
